8a53df670e9653176f3e779689a5fd0fa36638a7
vlmedvedev <@mail.ru>
2020-06-14 11:54:08 +0700

    add test AlreadyOcupiedException

--- src/io/hexlet/xo/controllers/MoveController.java
+++ src/io/hexlet/xo/controllers/MoveController.java
@@ -1,6 +1,5 @@
 package io.hexlet.xo.controllers;
 
-
 import io.hexlet.xo.model.Field;
 import io.hexlet.xo.model.Figure;
 import io.hexlet.xo.model.exceptions.AlreadyOccupiedException;
@@ -14,9 +13,6 @@ public class MoveController {
                             final Point point,
                             final Figure figure) throws InvalidPointException,
                                                         AlreadyOccupiedException {
-        if (field.getFigure(point) != null) {
-            throw new AlreadyOccupiedException();
-        }
         field.setFigure(point, figure);
     }
 
