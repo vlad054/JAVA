08fad29e8ea3999a41cb0571a7e4f388855012a9
vlmedvedev <@mail.ru>
2020-06-14 12:32:03 +0700

    add test AlreadyOcupiedException

--- src/io/hexlet/xo/controllers/WinnerController.java
+++ src/io/hexlet/xo/controllers/WinnerController.java
@@ -31,7 +31,6 @@ public class WinnerController {
         return null;
     }
 
-
     private boolean check(final Field field,
                           final Point currentPoint,
                           final IPointGenerator pointGenerator) {
